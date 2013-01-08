5b2b78019af08a03b576e39fdef3a336673f8ae9
landon9720 <landon9720@gmail.com>
2013-01-07 21:09:27 -0800

    tidy

--- src/main/scala/kuhn/Console.scala
+++ src/main/scala/kuhn/Console.scala
@@ -3,8 +3,6 @@ package kuhn
 import spray.can.client.HttpClient
 import spray.io._
 
-
-
 import akka.actor.{Props, ActorSystem}
 import spray.client.HttpConduit
 import HttpConduit._
@@ -13,42 +11,30 @@ import akka.dispatch.Await
 import akka.util.duration._
 
 import org.codehaus.jackson.map.ObjectMapper
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.codehaus.jackson.JsonNode
 
-//object RedditJsonProtocol extends DefaultJsonProtocol {
-//
-//	implicit object ThingFormat extends RootJsonFormat[Thing[_]] {
-//		def read(json:JsValue) = json match {
-//			case o:JsObject => o.fields.get("kind") match {
-//				case Some(JsString(Kinds.Listing)) => json.convertTo[Listing]
-//				case Some(JsString(Kinds.link)) => json.convertTo[Link]
-//				case Some(JsString(Kinds.comment)) => json.convertTo[Comment]
-//			}
-//		}
-//		def write(obj:Thing[_]) = null
-//	}
-//
-//	implicit val listingFormat:JsonFormat[Listing] = lazyFormat(jsonFormat3(Listing))
-//	implicit val linkFormat:JsonFormat[Link] = lazyFormat(jsonFormat15(Link))
-//	implicit val commentFormat:JsonFormat[Comment] = lazyFormat(jsonFormat15(Comment))
-//
-//
-//
-//}
+object MiscellaneousUtilities {
 
-//import RedditJsonProtocol._
+	val mapper = new ObjectMapper // is this thing threadsafe?
 
-object Tabs {
-	implicit def rich_tabs(s:String) = new {
+	implicit def rich_string(s:String) = new {
 		def tab = s.split("\n").mkString("  ", "\n  ", "")
+		def toJson = mapper.readTree(s)
 	}
-}
 
-import Tabs._
+	implicit def rich_json(j:JsonNode) = new {
+		def pretty = mapper.writerWithDefaultPrettyPrinter.writeValueAsString(j)
+	}
+
+}
+import MiscellaneousUtilities._
+
+// model //
 
 trait Thing {
 	val kind:String
+	val id:String
+	val name:String
 }
 
 object Thing {
@@ -58,7 +44,7 @@ object Thing {
 			case "more" => new More(json.path("data"))
 			case "t1" => new Comment(json.path("data"))
 			case "t3" => new Link(json.path("data"))
-			case _ => sys.error("What's this? %s".format(json.toString))
+			case _ => throw new RuntimeException("What's this?pn%s".format(json.pretty))
 		}
 	}
 }
@@ -73,22 +59,24 @@ object Listings {
 class Listing(json:JsonNode) extends Thing {
 
 	val kind = "Listing"
-	val before = json.path("before").asText
-	val after = json.path("after").asText
+	val id = "none"
+	val name = "none"
+	val before = Option(json.path("data").path("before").asText)
+	val after = Option(json.path("data").path("after").asText)
 
 	import collection.JavaConversions._
 	val things = for (thing <- json.path("data").path("children")) yield Thing(thing)
 
-	override def toString = "listing:\n" + things.mkString("\n").tab
+	override def toString = "listing (before=%s after=%s):\n".format(before.getOrElse("0"), after.getOrElse("0")) + things.mkString("\n").tab
 }
 
 class More(json:JsonNode) extends Thing {
 
 	val kind = "more"
+	val id = json.path("id").asText
+	val name = json.path("name").asText
 	val count = json.path("count").asInt
 	val parent_id = json.path("parent_id").asText
-	val id = json.path("parent_id").asText
-	val name = json.path("name").asText
 
 	import collection.JavaConversions._
 	val children = json.path("children").map(_.asText)
@@ -98,11 +86,13 @@ class More(json:JsonNode) extends Thing {
 
 class Comment(json:JsonNode) extends Thing {
 	val kind = "t1"
+	val id = json.path("id").asText
+	val name = json.path("name").asText
 	val body = json.path("body").asText
 	val link_id = json.path("link_id").asText
 	val replies = new Listing(json.path("replies"))
 
-	override def toString = "t1 (comment): %s\n%s".format(body, replies.toString.tab)
+	override def toString = "comment: %s\n%s".format(body, replies.toString.tab)
 }
 
 class Link(json:JsonNode) extends Thing {
@@ -113,8 +103,10 @@ class Link(json:JsonNode) extends Thing {
 	val title = json.path("title").asText
 	val ups = json.path("ups").asInt
 	val downs = json.path("downs").asInt
-	override def toString = "t3 (link): %s \"%s\" +%d -%d [%s]".format(url, title, ups, downs, name)
+	override def toString = "link: %s \"%s\" +%d -%d [%s]".format(url, title, ups, downs, name)
 }
+
+// runtime //
 
 object Console extends App {
 
@@ -125,88 +117,19 @@ object Console extends App {
 
 	val pipeline = sendReceive(conduit)
 
-
-	val mapper = new ObjectMapper()
-
-//	val jsonString = Await.result(pipeline(Get("/top/.json?limit=10")), 10 seconds).entity.asString
-//	val json = mapper.readTree(jsonString)
-//	println(mapper.writerWithDefaultPrettyPrinter.writeValueAsString(json))
-//
-//	val listing = new Listing(json)
-//	for (x <- listing.things) x match {
-//		case l:Link => println("URL: %s".format(l.url))
-//		case _ => println(x)
-//	}
-
-	val jsonString = Await.result(pipeline(Get("/comments/1632du.json?limit=3&depth=3&sort=top")), 10 seconds).entity.asString
+	val jsonString = Await.result(pipeline(Get("/top/.json?limit=20")), 10 seconds).entity.asString
 	val json = mapper.readTree(jsonString)
-	println(mapper.writerWithDefaultPrettyPrinter.writeValueAsString(json))
+	val listing = new Listing(jsonString.toJson)
+	println(listing)
 
-	def p(listing:Listing) {
-		println(listing)
-		for (m <- listing.things.collect { case m:More => m }) {
-			val link_id = listing.things.collectFirst { case c:Comment => c } map(_.link_id) get
-			val children = m.children.take(1).mkString(",")
-			val url = "/api/morechildren.json?link_id=%s&children=%s".format(link_id, children)
-			println("URL", url)
-			Thread.sleep(1000)
-			val jsonString = Await.result(pipeline(Post(url)), 10 seconds).entity.asString
-			println("more!\n" + mapper.writerWithDefaultPrettyPrinter.writeValueAsString(mapper.readTree(jsonString)))
+	def next(listing:Listing):Option[Listing] = {
+		for (after <- listing.after) yield {
+			new Listing(Await.result(pipeline(Get("/top/.json?limit=20&after=" + after)), 10 seconds).entity.asString.toJson)
 		}
 	}
+	println("NEXT!!")
+	println(next(listing))
 
-	Listings(json).foreach(p)
-
-
-
-
-//	val thing = json.convertTo[Thing[Listing[Link]]]
-//	for (x <- thing.data.children) {
-//		println("%s url: %s".format(x.data.id, x.data.url))
-//	}
-
-
-//
-//	val json = Await.result(pipeline(Get("/comments/1632du.json?limit=2&depth=2&sort=top")), 10 seconds).entity.asString.asJson
-//	println(json.prettyPrint)
-//
-//	val things = json.convertTo[List[Thing[_]]]
-//	for (t <- things) {
-//		println("THING")
-//		println(t)
-//	}
-
-//	for {
-//		thing <- things
-//		if thing.kind == Kinds.comment
-//		x <- thing.asInstanceOf[Thing[Comment]].data.children
-//	} {
-//		println("comment: %s".format(thing.data.toJson.prettyPrint))
-//		println("  comment: %s".format(x.data.toJson.prettyPrint))
-//	}
-
-
-
-//	println(thing)
-//
-//	println("raw:::")
-//	println(json.prettyPrint)
-
-
-//
-
-//
-//  val h = system.actorOf(Props[HelloWorld])
-//	import akka.pattern.ask
-//	for (result <- h ? "Landon") println(result)
-
-
-  Thread.sleep(1000)
-  system.shutdown
+	Thread.sleep(3000)
+	system.shutdown
 }
-
-//class HelloWorld extends Actor {
-//  protected def receive = {
-//    case name:String => sender ! "Hello, %s".format(name)
-//  }
-//}

3c9b70f4355983f4b8423f09dbfc00faa17fee3a
Landon Kuhn <landon@janrain.com>
2014-03-28 10:09:14 -0700

    front page test

--- src/main/scala/kuhn/Boot.scala
+++ src/main/scala/kuhn/Boot.scala
@@ -6,13 +6,17 @@ import spray.can.Http
 import akka.pattern.ask
 import akka.util.Timeout
 import scala.concurrent.duration._
+import uk.co.bigbeeconsultants.http.HttpBrowser
+import java.net.URL
+import spray.json._
+import DefaultJsonProtocol._
 
-object Boot extends App {
-  implicit val system = ActorSystem("on-spray-can")
-  val service = system.actorOf(Props[ServiceActor], "service-actor")
-  implicit val timeout = Timeout(5.seconds)
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 9797)
-}
+//object Boot extends App {
+//  implicit val system = ActorSystem("on-spray-can")
+//  val service = system.actorOf(Props[ServiceActor], "service-actor")
+//  implicit val timeout = Timeout(5.seconds)
+//  IO(Http) ? Http.Bind(service, interface = "localhost", port = 9797)
+//}
 
 import akka.actor.Actor
 import spray.routing._
@@ -42,7 +46,35 @@ class ServiceActor extends Actor with HttpService {
   }
 }
 
-object console {
+case class Page(
+  kind: String,
+  data: Data
+)
 
+case class Data(
+  children: List[Child]
+)
+
+case class Child(
+  kind: String,
+  data: ChildData
+)
+
+case class ChildData(
+  domain: String,
+  title: String
+)
+
+object console extends App {
+
+  implicit val childDataFormat = jsonFormat2(ChildData)
+  implicit val childFormat = jsonFormat2(Child)
+  implicit val dataFormat = jsonFormat1(Data)
+  implicit val pageFormat = jsonFormat2(Page)
+
+  val h = new HttpBrowser
+  val r = h.get(new URL("http://reddit.com/.json"))
+  private val x = r.body.asString.asJson.convertTo[Page]
+  println(x.data.children.map(_.data.title).mkString("\n"))
 
 }
